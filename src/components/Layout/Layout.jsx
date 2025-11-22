642dd691d77c0508d9b23864d4f92d9487f959da
Shiva Prasad <shivaprasadgopu209@gmail.com>
2025-11-22 19:47:08 +0530

    correction 2

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../Navbar/navbar";
+import Navbar from "../Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 
