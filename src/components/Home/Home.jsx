dc0a373106899a6cdb199b97f0da67ba5e8bbe7e
Shiva Prasad <shivaprasadgopu209@gmail.com>
2025-11-22 19:31:16 +0530

    3

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../Navbar/navbar";
+import Navbar from "../Navbar/Navbar";
 import Hero from "../Hero/Hero";
 import Category from "../Category/Category";
 import Values from "../Values/Values";
